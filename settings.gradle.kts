3a40f47235581be00c54925bd15f4faceda8711a
GitHub Action <action@github.com>
2022-08-18 13:15:29 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "today-assistant-plugin"
--- src/main/kotlin/com/github/taketoday/todayassistantplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/taketoday/todayassistantplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.taketoday.todayassistantplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.taketoday.todayassistantplugin.MyBundle
 
 class MyApplicationService {
 
