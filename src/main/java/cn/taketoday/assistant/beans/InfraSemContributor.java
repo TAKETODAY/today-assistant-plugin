3aca5fcde7c164a0bdca8985a967b35337cb34bb
Harry Yang <2228592280@qq.com>
2022-08-23 10:43:00 +0800

    :hammer: Update Copyright

--- src/main/java/cn/taketoday/assistant/beans/InfraSemContributor.java
+++ src/main/java/cn/taketoday/assistant/beans/InfraSemContributor.java
@@ -1,3 +1,23 @@
+/*
+ * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2022 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
 package cn.taketoday.assistant.beans;
 
 import com.intellij.openapi.project.Project;
@@ -62,12 +82,13 @@ import cn.taketoday.assistant.beans.stereotype.Component;
  * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
  * @since 1.0 2022/8/21 21:03
  */
-final class SpringSemContributor extends SemContributor {
+final class InfraSemContributor extends SemContributor {
 
   @Override
   public void registerSemProviders(SemRegistrar registrar, Project project) {
     PsiClassPattern nonAnnoClass = PsiJavaPatterns.psiClass().nonAnnotationType().withoutModifiers("private");
     PsiClassPattern classPattern = PsiJavaPatterns.psiClass().withoutModifiers("private");
+
     Component.META.register(registrar, nonAnnoClass.withAnnotation(AnnotationConstant.COMPONENT));
     SemService semService = SemService.getSemService(project);
     registerConfigurations(registrar, semService);
